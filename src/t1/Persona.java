f8cb43dbde4dcb8b9f661a7107956a5c123b1228
JeffersonMamani18 <142622187+JeffersonGMamaniT@users.noreply.github.com>
2023-09-08 13:55:27 -0500

    Add files via upload

---
+++ src/t1/Persona.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t1;
+
+/**
+ *
+ * @author Jefferson
+ */
+public class Persona {
+    private String nombre;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    
+}
